49b4731ae6044ca7f0f30a6f9ed6e2d0b4cef1fa
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2021-10-18 09:43:01 -0400

    ~40x faster to append list then add string

--- DTM to STL.py
+++ DTM to STL.py
@@ -1,5 +1,5 @@
 # By: Brendan Luke
-# Date: October 15, 2021
+# Date: October 16, 2021
 # Purpose: turn digital terrain models (DTMs) into 3D STL files (no facet normal)
 from datetime import datetime
 startTime = datetime.now()
@@ -41,7 +41,7 @@ lat = np.linspace(LatRange[0], LatRange[1],dims[0]-1)
 long = np.linspace(LongRange[0], LongRange[1],dims[1]-1)
 xLin, yLin, zLin = [], [], [] # declare linear x,y,z (for plotting)
 x, y, z = np.zeros((len(lat),len(long))), np.zeros((len(lat),len(long))), np.zeros((len(lat),len(long))) # declare x,y,z
-rowRemove, colRemove = 3, 1 # remove rows/columns of zeros data as needed
+rowRemove, colRemove = 0, 0#3, 1 # remove rows/columns of zeros data as needed
 startEndBool = bool(1) # remove from start(T,1) or end(F,0) of data
 for i in range(len(lat)-1-rowRemove):
     for j in range(len(long)-1-colRemove):
@@ -71,17 +71,17 @@ surf = ax.plot_trisurf(xLin, yLin, zLin)
 plt.axis('off')
 
 # Write ASCII STL File (no facet normal)
-stringOut = "solid " + FileName # initialize empty string to write STL file to
+stringOut = ["solid " + FileName] # initialize empty string to write STL file to
 for i in range(len(lat)-2-rowRemove):
     for j in range(len(long)-2-colRemove):
         # first triangle in 1x1 px square (top left)
-        stringOut += ("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
+        stringOut.append("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
                     +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i,j], vy = y[i,j], vz = z[i,j]) # top left point
                     +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i+1,j], vy = y[i+1,j], vz = z[i+1,j]) # bottom left point
                     +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i,j+1], vy = y[i,j+1], vz = z[i,j+1]) # top right point
                     +"endloop\n" + "endfacet")
         # second triangle in 1x1 px square (bottom right)
-        stringOut += ("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
+        stringOut.append("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
                     +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i+1,j+1], vy = y[i+1,j+1], vz = z[i+1,j+1]) # bottom right point
                     +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i,j+1], vy = y[i,j+1], vz = z[i,j+1]) # top right point
                     +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i+1,j], vy = y[i+1,j], vz = z[i+1,j]) # bottom left point
@@ -89,18 +89,19 @@ for i in range(len(lat)-2-rowRemove):
     print("Rows {:.1f}".format(100*i/(len(lat)-3-rowRemove))+"% complete")
 for i in range(len(lat)-2-rowRemove): # write end:start facets
     # first triangle in 1x1 px square (top left)
-    stringOut += ("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
+    stringOut.append("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
                 +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i,len(long)-3-colRemove], vy = y[i,len(long)-3-colRemove], vz = z[i,len(long)-3-colRemove]) # top left point
                 +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i+1,len(long)-3-colRemove], vy = y[i+1,len(long)-3-colRemove], vz = z[i+1,len(long)-3-colRemove]) # bottom left point
                 +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i,0], vy = y[i,0], vz = z[i,0]) # top right point
                 +"endloop\n" + "endfacet")
     # second triangle in 1x1 px square (bottom right)
-    stringOut += ("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
+    stringOut.append("\nfacet normal 0.0e+01 0.0e+01 0.0e+01\n" + "outer loop\n"
                 +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i+1,0], vy = y[i+1,0], vz = z[i+1,0]) # bottom right point
                 +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i,0], vy = y[i,0], vz = z[i,0]) # top right point
                 +"vertex {vx:e} {vy:e} {vz:e}\n".format(vx = x[i+1,len(long)-3-colRemove], vy = y[i+1,len(long)-3-colRemove], vz = z[i+1,len(long)-3-colRemove]) # bottom left point
                 +"endloop\n" + "endfacet")
-stringOut += "\nendsolid " + FileName # close out STL data
+stringOut.append("\nendsolid " + FileName) # close out STL data
+stringOut = ''.join(stringOut) # join list to string
 with open(dirname + "/" + FileName + ".stl", "w") as outFile:
     outFile.write(stringOut)
 
